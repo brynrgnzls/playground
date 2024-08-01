1e2c1b7d41057e36c0b9cf25ab4b3b911cf56c40
Bryan Gonzales <brynrgnzls@gmail.com>
2024-08-01 09:05:11 +0800

    [ADD] Fastify Chores

--- simple-fastify-server/src/app.ts
+++ simple-fastify-server/src/app.ts
@@ -1,13 +1,15 @@
 import { FastifyInstance, FastifyServerOptions } from "fastify";
 import { MessageRoute } from "./rotues/index.js";
+import fastifyCorsConfig from "./config/fastifyCorsConfig.js";
 export default function routes(
   instance: FastifyInstance,
   _option: FastifyServerOptions,
   done: Function
 ) {
-  instance.get("/test", async (req, res) => {
-    return res.send({ message: "Test This shit up", name: "Bryan Gonzales" });
-  });
+  instance.register(import("@fastify/cors"), fastifyCorsConfig),
+    instance.get("/test", async (req, res) => {
+      return res.send({ message: "Test This shit up", name: "Bryan Gonzales" });
+    });
   instance.register(MessageRoute, { prefix: "/message" });
 
   done();
